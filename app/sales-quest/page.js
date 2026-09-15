cc3a4117d51a487f2b2322ba0a317c1723aa4d49
whrshevchenko-art <whrshevchenko@gmail.com>
2026-09-15 16:08:18 +0900

    Update page.js

--- app/sales-quest/page.js
+++ app/sales-quest/page.js
@@ -23,9 +23,6 @@ export default function SalesQuestPage() {
   const endRef = useRef(null);
 
   useEffect(() => setUserId(getUserId()), []);
-  useEffect(() => {
-    endRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [messages, loading]);
   
   async function request(url, body) {
     const response = await fetch(url, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(body) });
